a5f7c8c850789d82140e2260bd37a234cfca258c
Satvik <satviksethia@gmail.com>
2018-09-24 00:26:47 -0500

    Makeover with new file structure

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function main() {
+  $(".title").hide();
+  $(".text").hide();
+  
+  $(".title").fadeIn(3500);
+  $(".text").fadeIn(4000);
+
+  myMove();
+
+  function myMove() {
+      var elem = document.getElementById("dot-1");   
+      var pos = 0;
+      var id = setInterval(frame, 5);
+      function frame() {
+        if (pos == 350) {
+          clearInterval(id);
+        } else {
+          pos++; 
+          elem.style.top = pos + 'px';
+        }
+      }
+    }
+}
+$(document).ready(main);
