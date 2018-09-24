7d58d0c168338ea77d463004dd00dc3116ad53a9
Satvik <satviksethia@gmail.com>
2018-09-24 00:57:29 -0500

    Trying new bg colors

--- js/main.js
+++ js/main.js
@@ -4,21 +4,6 @@ function main() {
   
   $(".title").fadeIn(3500);
   $(".text").fadeIn(4000);
-
-  myMove();
-
-  function myMove() {
-      var elem = document.getElementById("dot-1");   
-      var pos = 0;
-      var id = setInterval(frame, 5);
-      function frame() {
-        if (pos == 350) {
-          clearInterval(id);
-        } else {
-          pos++; 
-          elem.style.top = pos + 'px';
-        }
-      }
-    }
 }
+
 $(document).ready(main);
