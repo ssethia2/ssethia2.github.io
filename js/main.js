8f008c8af96f29560da43d1237a23c74e724940d
Satvik <satviksethia@gmail.com>
2018-10-11 00:11:06 -0500

    Fixed scrolling js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 function main() {
-  $(".title").hide();
   $(".text").hide();
   
-  $(".title").fadeIn(3500);
   $(".text").fadeIn(4000);
+  
+  $("#nav-home").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".main").offset().top
+    }, 2000);
+  });
+
+  $("#nav-about").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".c_bio").offset().top
+    }, 2000);
+  });
+  
+  $("#nav-link").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".resume").offset().top
+    }, 2000);
+  });
 }
 
 $(document).ready(main);
