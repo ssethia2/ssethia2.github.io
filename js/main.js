a3d9a8918a8da55ba6f733eec5726278d9ba0b23
Satvik Sethia <satviksethia@gmail.com>
2020-02-06 22:32:34 -0500

    Minor changes, update resume

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 function main() {
   $(".text").hide();
-  
+
   $(".text").fadeIn(4000);
-  
+
   $("#nav-home").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $(".main").offset().top
-    }, 2000);
+    }, 1000);
   });
 
   $("#nav-about").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $(".c_bio").offset().top
-    }, 2000);
+    }, 1000);
   });
-  
+
   $("#nav-link").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $(".resume").offset().top
-    }, 2000);
+    }, 1000);
   });
 }
 
